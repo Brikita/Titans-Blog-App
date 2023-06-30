b006f91b88c0221c8df3876e6ceb61383ca29a77
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-30 09:42:47 +0300

    commit 4 add asome content

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,1 +1,25 @@
 import React from 'react';
+import Blog from "./Blog"
+
+
+export default function Profile(){
+
+
+
+    const [blog, setBlog] = React.useState({
+        id: "",
+        content: "",
+        author: "",
+        coverImg: "",
+        time: "",
+        tags: []
+    });
+
+     
+
+    return (
+        <div>
+            <Blog />
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import  React  from 'react'
+import Profile from '../components/Profile'
 
 
 export default function App() {
-
+    
+    return (
+    <>
+        <Profile />
+    </>
+    )
 }

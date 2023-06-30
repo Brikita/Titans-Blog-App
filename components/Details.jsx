ef3de2267d121c82fcc6e2902dd2557077b38b5a
patrickleon710 <134448809+patrickleon710@users.noreply.github.com>
2023-06-30 13:56:32 +0300

    adding functions and more styling

--- components/Details.jsx
+++ components/Details.jsx
@@ -1,14 +1,15 @@
 import React from "react"
 
-export default function Details() {
+export default function Details(props) {
     return (
         <div className="profile">
             <div className="image-name">
                 <img src="../images/blank-profile-picture-973460_1280.webp" className="profile--image"/>
-                <h2 className="profile--name">Patrick</h2>
-            </div>
-            <p className="profile--description">Description...</p>
-            <button className="profile-edit-button">Edit Profile</button>
+                {props.nameInput}
+            </div>            
+                <h3 className="bio--title">Bio</h3>
+                {props.descriptionInput}
+            <button onClick={() => props.handleClick()} className="profile-edit-button">Edit Profile</button>
         </div>
     )
 }
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -3,10 +3,73 @@ import Details from "./Details"
 import Header from "./Header"
 
 export default function Profile() {
-    return (
-        <div>
-            <Header />
-            <Details />
-        </div>
+    // STATES
+    const [profile, setProfile] = React.useState(
+        JSON.parse(localStorage.getItem("profile")) ||
+        {profileName: "", description: ""}
+    )  
+    const [editing, setEditing] = React.useState(false);
+
+    // STORING TO LOCAL STORAGE
+    React.useEffect(function() {
+        localStorage.setItem("profile", JSON.stringify(profile))
+    }, [profile])
+    
+    
+    // FUNCTIONS TO HANDLE EVENTS
+    function handleChange(event) {              
+        return (
+            setProfile(prevProfile => {
+                return {
+                    ...prevProfile,
+                    [event.target.name]: event.target.value
+                }
+            })
+        )
+    }           
+            
+    
+    // CLICK FUNCTION
+    function editProfile() {
+        setEditing(prevEditing => !prevEditing)
+    }
+        
+        
+
+        return (
+            <div>
+        <Header />
+        <Details 
+        nameInput={
+            editing ? (
+                <h2 className="profile-name">{profile.profileName}</h2>
+            ) : (
+                <input 
+                    className="profile--name"
+                    name="profileName" 
+                    type="text" 
+                    placeholder="Your Name" 
+                    value={profile.profileName} 
+                    onChange={handleChange}
+                ></input>
+            )
+        }
+        descriptionInput={
+            editing ? (
+                <p className="profile--paragraph">{profile.description}</p>
+            ) : (
+                <input
+                    className="profile--description" 
+                    name="description"
+                    type="text" 
+                    placeholder="Write your bio" 
+                    value={profile.description} 
+                    onChange={handleChange}
+                ></input>
+            )
+        }
+        handleClick={editProfile}
+        />
+    </div>
     )
-}
+    }

5bda02ef1adf3bd171f0e6db46480a247805820e
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-30 12:42:52 +0300

    commit 7 add some logic

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 
-export default function Blog() {
+export default function Blog(props) {
+    /* const newBlog = {
+        id: "",
+        content: "",
+        author: "",
+        coverImg: "",
+        time: "",
+        tags: []
+    } */
+    
+
 
     
     return (
         <div className='blog-container'>
             <fieldset>
                 <legend>Write a new blog</legend>
-                <textarea type="text" className='blog-input' placeholder='Write your Blog here' name='content'/>
+                <textarea type="text" className='blog-input' placeholder='Write your Blog here' name='content' onChange={props.changeContent} value={blog[0].content}/>
 
                 <button className='blog-post-btn'>Post Blog</button>
             </fieldset>
--- components/Profile.jsx
+++ components/Profile.jsx
@@ -3,23 +3,52 @@ import Blog from "./Blog"
 
 
 export default function Profile(){
+    const [blog, setBlog] = React.useState(
+        JSON.parse(localStorage.getItem("blogData")) || [] );
+    React.useEffect(() => {
+        localStorage.setItem("blogData", JSON.stringify(blog))
+        
+
+    }, [blog])
+
+    function newBlog() {
+        const blogItem = {
+            id: "",
+            content: "",
+            author: "",
+            coverImg: "",
+            time: "",
+            tags: []
+        }
+      setBlog(prevBlog => [blogItem, ...prevBlog])  
+    }
+    
+    //const [blogContent, setBlogContent] = React.useState(JSON.parse(localStorage.getItem("blogData")) || {newBlog})
+
+    
+
+    
+    
+
+    function changeContent(event) {
+        const {value, name} = event.target
+        setBlog(prevBlog => prevBlog.map(blogItem => {
+            return {...prevBlog, content: value}
+        }))
+        
+        console.log(blog.content)
+    }
 
 
-
-    const [blog, setBlog] = React.useState({
-        id: "",
-        content: "",
-        author: "",
-        coverImg: "",
-        time: "",
-        tags: []
-    });
-
+    
      
 
     return (
         <div>
-            <Blog />
+            <Blog 
+                createBlog = {newBlog}
+                changeContent={changeContent}
+            />
         </div>
     )
 }
